fd0fce89efd385108fc4d3ce683b7a59501e2710
anvbis <stephen.t.radley+anvbis@protonmail.com>
2021-12-25 13:46:00 +1100

    cleaned up code

--- 1-finding-kernel32-dll/shellcode.py
+++ 1-finding-kernel32-dll/shellcode.py
@@ -4,27 +4,23 @@ from pwn import *
 
 
 shellcode = asm('''
-    int3
-
 start:
     mov ebp, esp
     sub sp, 0x60
 
-find_kernel32:
+get_init_order_module_list:
     xor ecx, ecx
     mov esi, fs:[ecx+0x30]
     mov esi, [esi+0x0c]
     mov esi, [esi+0x1c]
 
-next_module:
+check_module:
     mov ebx, [esi+0x08]
     mov edi, [esi+0x20]
     mov esi, [esi]
     cmp [edi+0x18], cx
-    jne next_module
-
-    int3
+    jne check_module
 ''')
 
-print(disasm(shellcode))
-print(shellcode)
+print(f'\n{disasm(shellcode)}')
+print(f'\n  shellcode = {shellcode}\n')
